a6acee892867f15f04191615e22ed9224e580f22
matteomaggioni@tiscali.it <matteomaggioni@tiscali.it@3beca010-3ff3-11de-b232-599c1b3c59e8>
2009-05-16 19:44:12 +0000

    -better user interaction

--- trunk/ias721923.m
+++ trunk/ias721923.m
@@ -1,23 +1,39 @@
-% create example image
-image = double(imread('image/peppers.png'));
+%% parameters definition
 
-% add zero-mean white gaussian noise
-sigma = 20;
-frames = 4;
-randn('seed', 0);
-for i = 1:frames
-    noisy_images(:,:,i) = image + sigma*randn(size(image)); %#ok<AGROW>
-    %noisy_images(:,:,i) = double(imnoise(image, 'gaussian', 0, sigma/100)); %#ok<AGROW>
+% frames number
+max_frames = 1;
+% noise definition
+sigma = 15;
+randn('state', 0);
+% show verbose output
+verbose = true;
+% show graphically algorithm execution
+graphic = false;
+% create example image
+image = double(imread('image/barbara.png'));
+
+%% execution
+
+for f = 1:max_frames
+    clear noisy_images;
+    % creating the proper number of noisy images
+    for i = 1:f
+        noisy_images(:,:,i) = image + sigma*randn(size(image)); %#ok<AGROW>
+    end
+
+    % denoise it
+    nl_images(:,:,f) = non_local_means(noisy_images, 7, 3, sigma, verbose, graphic); %#ok<AGROW>
+
+    %[mse, psnr] = statistics(image, noisy_image);
+    psnr(f) = statistics(image, nl_images(:,:,f)); %#ok<AGROW>
+
+    % show results
+    figure(1);
+    subplot(1,2,1), imshow(image, []), title('original');
+    subplot(1,2,2), imshow(nl_images(:,:,f), []), title('nl denoised');
 end
 
-% denoise it
-verbose = false;
-nl_image = non_local_means(noisy_images, 7, 3, sigma, verbose);
-
-%[mse, psnr] = statistics(image, noisy_image);
-nl_psnr = statistics(image, nl_image);
-
-% show results
-figure(1);
-subplot(1,2,1), imshow(image, []), title('original');
-subplot(1,2,2), imshow(nl_image, []), title('nl denoised');
+%% plotting
+if max_frames>1
+    plot(1:max_frames, psnr);
+end
