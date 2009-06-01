11a35e563c07d3b6113d26492ef3de740987f57c
matteomaggioni <matteomaggioni@tiscali.it>
2009-06-01 19:33:57 +0000

    -more transformation to image -possibility to change noise type -better tester

--- multi_frame_denoise.m
+++ multi_frame_denoise.m
@@ -1,62 +1,38 @@
-function [ nl_image, psnr, time ] = multi_frame_denoise( image, noisy_images, win, neig, h, transformation, use_mex )
+function [ nl_image, time ] = multi_frame_denoise( noisy_images, win, neig, h, use_mex )
     % MULTI_FRAME_DENOISE
     %   Returns an image denoised with the non local means algorithm 
     %   starting from a series of noisy frames depicting the same image.
     %   
-    %   Use: [nl_image, psnr, time] = multi_frame_denoise( image, noisy_images, win, neig, h, use_mex )
+    %    [ nl_image, time ] = multi_frame_denoise( noisy_images, win, neig, h, use_mex )
     %   
-    %   image           original image
     %   noisy_images    an array of gray-scale images
     %   win             half-size of the search window
     %   neig            half-size of the neighborhood
     %   h               degree of filtering, it controls the decay of the
     %                   weights as a function of the Euclidean distances
-    %   transformation  struct containig parameters to transform image
     %   use_mex         use mex file
     %
     %
     %   Matteo Maggioni - Spring 2009
-    
-    % add transformation to images
-    noisy_images = transform_image(noisy_images, transformation);
-    
-    [heigth width frames] = size(noisy_images);
-    
-    % preallocating objects
-    noisy_images_padded = zeros(heigth+neig*2, width+neig*2, frames);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     tic;
-    
-    disp(sprintf('Start denoising with %d frames', frames));
-    disp(sprintf('\timage size: %dx%d pixel', width, heigth));
-    disp(sprintf('\tsearch (similarity) window: %dx%d pixel', win*2+1, win*2+1));
-    disp(sprintf('\tneighborhood window: %dx%d pixel', neig*2+1, neig*2+1));
-    disp(sprintf('\tnoise standard deviation: %d', h));
-    disp(sprintf('\ttransformation type: %s', transformation.type));
-    
+
     start = toc;
     
     % computing gaussian kernel of the same size of neighborhood windows
-    kernel = gaussian_kernel(neig, 1);
+    kernel = gaussian_kernel(neig);
     
     % padding image to let boundary pixels have a proper neighborhood
-    for i = 1:frames
-        noisy_images_padded(:,:,i) = padarray(noisy_images(:,:,i), [neig neig], 'replicate');
-    end
+    noisy_images_padded = padarray(noisy_images, [neig neig], 'replicate');
     
     % running algorithm
     if use_mex
-        disp(sprintf('\tusing MEX file'));
         nl_image = non_local_means_mex(noisy_images_padded, kernel, win, neig, h);
     else
-        disp(sprintf('\tusing MAT file'));
         nl_image = non_local_means(noisy_images_padded, kernel, win, neig, h);
     end
     
-    psnr = statistics(image, nl_image);
+    time = toc - start;
     
-    time = ceil(toc - start);
-    
-    disp(sprintf('\tpsnr: %g dB', psnr));
-    disp(sprintf('\texecution time: %d seconds\n', time));
 end
---
+++ transform_images.m
@@ -0,0 +1,124 @@
+function [ noisy_images, sigma ] = transform_images( original_images, original_sigma, transformation )
+    % TRANSFORM
+    %   Generates a series of noisyimages rotated or translated starting 
+    %   from the original image passed as input parameter.
+    %
+    %    [ noisy_images, sigma ] = transform_images( original_images, original_sigma, transformation )
+    %
+    %   original_images    an array of images
+    %   original_sigma     original standard deviation of noise
+    %   transformation     a struct defining transformation parameters, see
+    %                      following skeleton for details
+    %
+    %   struct(    
+    %     'type','oracle'|'rotated'|'translated'|'shaked'|'scaled'|'fixed',
+    %     'randomize', true|false,
+    %     'degree', int,
+    %     'tx',int,
+    %     'ty',int,
+    %     'minscale',int,
+    %     'maxscale',int
+    %   );
+    %
+    %   If 'randomixed' is set to false then 'maxscale' will be used for 
+    %   scaling. If 'randomize' is true then each value will be set equal
+    %   to a random uniformly distributed number between 0 and the actual
+    %   value passed in struct.
+    %
+    %
+    %   Matteo Maggioni - Spring 2009
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    [heigth width frames] = size(original_images);
+    noisy_images = original_images;
+    
+    sigma = original_sigma;
+    
+    type = lower(transformation.type);
+    
+    for i = 1:frames
+        
+        if strcmp(type, 'rotated') || strcmp(type, 'shaked')
+            % picking sign of the rotation
+            if rand>0.5
+                sign = 1;
+            else
+                sign = -1;
+            end
+            % picking value of the rotation
+            if transformation.randomize
+                degree = sign*transformation.degree*rand;
+            else
+                degree = transformation.degree;
+            end
+
+            noisy_images(:,:,i) = imrotate(noisy_images(:,:,i), degree, 'crop');
+        end
+    
+        if strcmp(type, 'translated') || strcmp(type, 'shaked')
+            % picking signs of the translation
+            if rand>0.5
+                signx = 1;
+            else
+                signx = -1;
+            end
+            if rand>0.5
+                signy = 1;
+            else
+                signy = -1;
+            end
+            % picking values of the translation
+            if transformation.randomize
+                tx = signx*round(transformation.tx*rand);
+                ty = signy*round(transformation.ty*rand);
+            else
+                tx = transformation.tx;
+                ty = transformation.ty;
+            end
+            T = maketform('affine', [1 0 0; 0 1 0; tx ty 1]);
+            
+            noisy_images(:,:,i) = imtransform(noisy_images(:,:,i), T, 'XData',[1 width], 'YData',[1 heigth]);
+        end
+        
+        if strcmp(type, 'scaled') || strcmp(type, 'shaked')
+            % picking values of scaling
+            if transformation.randomize
+                scale = (transformation.maxscale-transformation.minscale)*rand + transformation.minscale;
+            else
+                scale = transformation.maxscale;
+            end
+            cropped_image = zeros(heigth, width);
+            scaled_image = imresize(uint8(noisy_images(:,:,i)), scale);
+            [sh sw] = size(scaled_image);
+            
+            if sh~=heigth || sw~=width
+                % when scale factor is cole to one, the scale has no effect
+                if scale>1
+                    % scaled image is bigger than original
+                    bitw = uint8(~mod(sw - width, 2));
+                    bith = uint8(~mod(sh - heigth, 2));
+
+                    ow = ceil((sw - width)/2);
+                    oh = ceil((sh - heigth)/2);
+
+                    cropped_image = scaled_image(oh:sh-oh-bith, ow:sw-ow-bitw);
+                else
+                    % scaled image is smaller than original
+                    ow = ceil((width - sw)/2);
+                    oh = ceil((heigth - sh)/2);
+
+                    cropped_image(oh:sh+oh-1, ow:sw+ow-1) = scaled_image;
+                end
+                noisy_images(:,:,i) = double(cropped_image);
+            end
+            
+        end
+        
+    end
+    
+    if strcmp(transformation.type, 'oracle')
+        noisy_images = mean(noisy_images, 3);
+        sigma = original_sigma / sqrt(frames);
+    end
+    
+end
