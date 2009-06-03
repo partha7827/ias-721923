32d45708d99d96bf3bad10762b251cebb87cf18f
matteomaggioni@tiscali.it <matteomaggioni@tiscali.it@3beca010-3ff3-11de-b232-599c1b3c59e8>
2009-06-03 17:32:52 +0000

    -added poiss&gauss noise

--- trunk/add_noise.m
+++ trunk/add_noise.m
@@ -1,28 +1,60 @@
-function [ noisy_image, noise_data ] = add_noise( original_image, noise, sigma )
+function [ noisy_image, noise_data ] = add_noise( original_image, noise_type, varargin )
     % ADD_NOISE
     %   Add a certain type of noise to the specified image:
     %   
-    %    [ noisy_image ] = add_noise( original_image, noise, sigma )
+    %    [ noisy_image ] = add_noise( original_image, noise_type, sigma, a, b, clip )
     %
     %   original_image  the image to which add noise
-    %   noise           a string containing the noise type
-    %   sigma           noise standard deviation
+    %   noise_type      a string containing the noise type, it can be one
+    %                   of the following values: 'gaussian', 'poisson', 
+    %                   'poiss & gauss', 'salt & pepper', 'speckle'
+    %   a               semantic varies in function of the noise type
+    %   b               gaussian noise standard deviation
+    %   clip            choose whether to clip pixel vlue or not (only in 
+    %                   case of 'poiss & gauss' noise)
     %
     %
     %   Matteo Maggioni - Spring 2009
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
-    switch noise
+    optargs = size(varargin,2);
+    
+    if optargs>=1
+        a = varargin{1};
+    end
+    if optargs>=2
+        b = varargin{2};
+    end
+    if optargs==3
+        clip = varargin{3};
+    end
+    
+    switch noise_type
         case 'gaussian'
-            noisy_image = imnoise(original_image, 'gaussian', 0, sigma^2);
+            noisy_image = imnoise(original_image, 'gaussian', a, b^2);
         case 'poisson'
             noisy_image = imnoise(original_image, 'poisson');
+        case 'poiss & gauss'
+            
+                noisy_image = zeros(size(original_image));
+                
+                if a~=0
+                    chi = 1/a;
+                    noisy_image = poissrnd(max(0,chi*original_image)) / chi + min(original_image,0);
+                end
+
+                noisy_image = imnoise(noisy_image, 'gaussian', 0, b^2);
+
+                if clip
+                    noisy_image = min(noisy_image,1);
+                    noisy_image = max(0,noisy_image);
+                end
         case 'salt & pepper'
-            noisy_image = imnoise(original_image, 'salt & pepper', sigma);
+            noisy_image = imnoise(original_image, 'salt & pepper', a);
         case 'speckle'
-            noisy_image = imnoise(original_image, 'speckle', sigma);
+            noisy_image = imnoise(original_image, 'speckle', a);
         otherwise
-            noisy_image = imnoise(original_image, 'gaussian', 0, sigma^2);
+            noisy_image = imnoise(original_image, 'gaussian', a, b^2);
     end
     
     noise_data = original_image - noisy_image;
--- trunk/ias721923.m
+++ trunk/ias721923.m
@@ -6,8 +6,10 @@ clc
 
 % maximum number of frames in array
 max_frames = 4;
-% noise standard deviation
-sigma = 0.05;
+% noise definition
+a = 1/200;
+b = 10/255;
+clip = true;
 % boolean - set to true to use mex file
 use_mex = true;
 % search window halved size
@@ -16,7 +18,7 @@ win = 7;
 neig = 3;
 
 % read example image - values range from 0 to 1
-image = im2double(imread('image/barbara.png'));
+image = im2double(imread('image/digest.png'));
 [heigth width] = size(image);
 
 % comment out the transformations you don't want to test
@@ -40,9 +42,9 @@ transformation = struct( ...
 );
 
 % type of noise
-noise = {'gaussian', 'poisson', 'salt & pepper', 'speckle'};
+noise = {'gaussian', 'poisson', 'poiss & gauss', 'salt & pepper', 'speckle'};
 % index of the noise type to use - 1:gaussian, 2:poisson, etc...
-index = 1;
+index = 3;
 
 
 
@@ -52,7 +54,7 @@ disp(sprintf('image size: %dx%d pixel', width, heigth));
 disp(sprintf('search (similarity) window: %dx%d pixel', win*2+1, win*2+1));
 disp(sprintf('neighborhood window: %dx%d pixel', neig*2+1, neig*2+1));
 disp(sprintf('noise type: %s', char(noise(index))));
-disp(sprintf('noise standard deviation: %g', sigma));
+disp(sprintf('noise defintion: a=%g b=%g clip=%d', a, b, clip));
 if use_mex
     disp(sprintf('using MEX file'));
 else
@@ -65,7 +67,7 @@ time = zeros(max_frames, length(type));
 for f = 1:max_frames
     
     % creating the proper number of noisy images
-    [noisy_images(:,:,f) noise_data] = add_noise(image, char(noise(index)), sigma); %#ok<AGROW>
+    [noisy_images(:,:,f) noise_data] = add_noise(image, char(noise(index)), a, b, clip); %#ok<AGROW>
     
     for i = 1:length(type)
         % set the transformaion type
@@ -75,7 +77,7 @@ for f = 1:max_frames
         disp(sprintf('\tnumber of frames: %d', f));
         
         % add transformation to images
-        [messy_images h] = transform_images(noisy_images, sigma, transformation);
+        [messy_images h] = transform_images(noisy_images, b, transformation);
         
         % denoise it
         [nl_images(:,:,f,i) time(f,i)] = multi_frame_denoise(messy_images, win, neig, h, use_mex); %#ok<AGROW>
@@ -88,7 +90,7 @@ for f = 1:max_frames
         
         % show results
         fig = figure(1);
-        set(fig, 'Name', sprintf('Denoising result with %d frames - %s sequence', f, transformation.type), 'NumberTitle','Off');
+        set(fig, 'Name', sprintf('%d frames - %s sequence - %s noise', f, transformation.type, char(noise(index))), 'NumberTitle','Off');
         subplot(2,2,1), imshow(image, []), title('original');
         subplot(2,2,2), imshow(noise_data, []), title('noise');
         subplot(2,2,3), imshow(nl_images(:,:,f,i), []), title('nl denoised');
