9345ae12245379e7336bd49b520b5407e6615511
matteomaggioni <matteomaggioni@tiscali.it>
2009-06-04 22:13:43 +0000

    -fixed MEX file - performace is as good as the MAT file

--- non_local_means_mex.c
+++ non_local_means_mex.c
@@ -22,8 +22,9 @@ double getN1Value(int i, int j, int m, int n) {
     return noisy[i+m+(j+n)*(heigth+2*neig)];
 }
 
-double getN2Value(int r, int c, int m, int n, int f) {
-    return noisy[(r-neig+m)+(c-neig+n)*(heigth+2*neig) + f*(width+2*neig)*(heigth+2*neig)];
+double getN2Value(int r, int c, int m, int n, int k) {
+    int index = (r-neig+m)+(c-neig+n)*(heigth+2*neig) + k*(width+2*neig)*(heigth+2*neig);
+    return noisy[index];
 }
 
 
@@ -43,12 +44,14 @@ double getN2Value(int r, int c, int m, int n, int f) {
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     
     mwSize num_of_dims = mxGetNumberOfDimensions(prhs[0]);
+    mwSize num_of_elem = mxGetNumberOfElements(prhs[0]);
     const mwSize *dims = mxGetDimensions(prhs[0]);
     
     double *nl_image;
     
-    int i, j, r, c, col_min, col_max, row_min, row_max;
-    int f, m, n;
+    int i, j;
+    int col_min, col_max, row_min, row_max;
+    int r, c, m, n, k;
     double z, nl, mw, gwed, w;
     
     noisy = mxGetPr(prhs[0]);
@@ -63,6 +66,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     
     plhs[0] = mxCreateDoubleMatrix(heigth, width, mxREAL);
     nl_image = mxGetPr(plhs[0]);
+    
+    //mexPrintf("%d x %d x %d = %d\n", dims[1], dims[0], frames, num_of_elem);
     
     for(i=0; i<heigth; i++) {
         for(j=0; j<width; j++) {
@@ -83,26 +88,28 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
                 for(c=col_min; c<=col_max; c++) {
                     
                     // for each frame
-                    for(f=0; f<frames; f++) {
+                    for(k=0; k<frames; k++) {
                         
-                        gwed = 0;
-                        
-                        // for each neighborhood in search window
-                        for(m=0; m<2*neig+1; m++) {
-                            for(n=0; n<2*neig+1; n++) {
-                                gwed += getKernelValue(m, n) * pow(getN1Value(i, j, m, n) - getN2Value(r, c, m, n, f), 2);
+                        if(k!=0 || !(r==i+neig && c==j+neig)) {
+                            gwed = 0;
+
+                            // for each neighborhood in search window
+                            for(m=0; m<=2*neig; m++) {
+                                for(n=0; n<=2*neig; n++) {
+                                    gwed += getKernelValue(m, n) * pow(getN1Value(i, j, m, n) - getN2Value(r, c, m, n, k), 2);
+                                }
                             }
+
+                            w = exp(-gwed / pow(h, 2));
+
+                            if(w>mw) {
+                                mw = w;
+                            }
+
+                            z = z + w;
+
+                            nl = nl + w*noisy[r + c*(heigth+2*neig) + k*(width+2*neig)*(heigth+2*neig)];
                         }
-                        
-                        w = exp(-gwed / pow(h, 2));
-                                
-                        if(w>mw) {
-                        	mw = w;
-                        }
-                        
-                        z = z + w;
-                        
-                        nl = nl + w*noisy[r + c*(heigth+2*neig)];
                         
                     }
                     
